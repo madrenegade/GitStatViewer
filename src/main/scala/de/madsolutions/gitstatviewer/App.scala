4a74d99c34cff92b6de0f648582b3c1c962464e4
Stefan Bradl <madrenegade@gmx.de>
2011-05-16 08:20:20 +0200

    Added class skeleton

--- src/main/scala/de/madsolutions/gitstatviewer/App.scala
+++ src/main/scala/de/madsolutions/gitstatviewer/App.scala
@@ -8,7 +8,40 @@ package de.madsolutions.gitstatviewer
 object App {
 
   def main(args: Array[String]) {
-    println("Hello from Scala")
+    val directory = args match {
+      case Array() => {
+          println("No diretory specified - using current working directory")
+          "."
+      }
+      case Array(x) => {
+          println("using specified directory")
+          x
+      }
+      case _ => {
+          println("Invalid number of arguments")
+          sys.exit(1)
+      }
+    }
+    
+    val logData = LogFetcher.fetch(directory) match {
+      case Some(x) => x
+      case None => sys.exit(2)
+    }
+    
+    val logExtractor = new LogExtractor
+    
+    val log = logExtractor.extractFrom(logData) match {
+      case Some(x) => x
+      case None => sys.exit(3)
+    }
+    
+    val analyzer = new LogAnalyzer
+    
+    val statistics = analyzer.analyze(log) match {
+      case Some(x) => x
+      case None => sys.exit(4)
+    }
+    
   }
   
 }
---
+++ src/main/scala/de/madsolutions/gitstatviewer/LogExtractor.scala
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.madsolutions.gitstatviewer
+
+class LogExtractor {
+
+  def extractFrom(logData: String): Option[Log] = {
+    None
+  }
+  
+}
