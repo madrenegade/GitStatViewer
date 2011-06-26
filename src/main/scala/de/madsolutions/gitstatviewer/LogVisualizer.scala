e052dc228a918de0b1604ab3911ec4e8461832ed
Stefan Bradl <madrenegade@gmx.de>
2011-06-26 12:15:21 +0200

    OPEN - task 1: Report Layout http://github.com/madrenegade/GitStatViewer/issues/issue/1
    Changed doctype of generated report to html5

--- src/main/scala/de/madsolutions/gitstatviewer/LogVisualizer.scala
+++ src/main/scala/de/madsolutions/gitstatviewer/LogVisualizer.scala
@@ -9,8 +9,14 @@ import de.madsolutions.reports.generator.ReportGenerator
 import java.io.File
 import scala.xml.Elem
 import scala.xml.XML
+import scala.xml.EntityRef
+import scala.xml.dtd.DocType
+import scala.xml.dtd.SystemID
 
 class LogVisualizer(stats: Elem) {
+  
+  private val indexFile = "report.html"
+  private val backHref = "../" + indexFile
   
   protected val reporters: List[ReportGenerator] = List()
 
@@ -22,36 +28,65 @@ class LogVisualizer(stats: Elem) {
                               <div>{
           reporters map {
             generator: ReportGenerator => {
-              // create xhtml from this
               val dir = new File(outputPath + "/" + generator.name)
               dir.mkdir
 
               val partialReport = <div>
                 {generator.generateReport(outputPath + "/" + generator.name, stats)}
                 <hr />
-                <a href="../report.xhtml">Back</a>
+                <a href={backHref}>Back</a>
               </div>
 
-              XML.save(outputPath + "/" + generator.name + "/report.xhtml", generatePage(generator.name, partialReport), "UTF-8", xmlDecl = true)
+              XML.save(outputPath + "/" + generator.name + "/" + indexFile, generatePage(generator.name, partialReport), "UTF-8", xmlDecl = true)
 
               <a href={reportUrl(generator.name)}>{generator.name}</a><br />
             }
           }
         }</div>)
 
-    XML.save(outputPath + "/report.xhtml", report, "UTF-8", xmlDecl = true)
+    XML.save(outputPath + "/" + indexFile, report, "UTF-8", xmlDecl = false, new DocType("html", SystemID("about:legacy-compat"), Nil))
   }
 
-  private def reportUrl(name: String) = "./" + name + "/report.xhtml"
+  private def reportUrl(name: String) = "./" + name + "/" + indexFile
 
   private def generatePage(title: String, body: Elem) = {
-    <html xmlns="http://www.w3.org/1999/xhtml">
+    <html lang="en">
       <head>
         <title>{title}</title>
+        <style type="text/css">
+        	html, body, div, p {{
+        		margin: 0;
+        		padding: 0;
+        		border: 0;
+        	}}
+
+        	.main {{
+        		margin:0 auto; width:85%;
+        	}}
+
+        	.dp20, .dp25, .dp33, .dp50, .dp100 {{
+        		float:left; display: inline; *margin-left:-0.04em;
+        	}}
+
+        	.dp20{{width:20%;}}
+        	.dp25{{width:25%;}}
+        	.dp33{{width:33.33%;}}
+        	.dp50{{width:50%;}}
+        	.dp100{{width:100%;}}
+        	.clear{{ clear:both;}}
+        </style>
       </head>
       <body>
-        <h1>{title}</h1>
-        {body}
+        <div class="main">
+        	<div class="dp100"><h1>{title}</h1></div>
+        	<div class="dp100">{body}</div>
+        	<div class="dp100">
+        		<h3>
+        			<span style="font-style: italic">GitStatViewer</span>
+        			{EntityRef("#169")} 2011 Stefan Bradl
+        		</h3>
+        	</div>
+        </div>
       </body>
     </html>
   }
