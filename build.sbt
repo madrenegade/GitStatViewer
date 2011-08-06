3051cbf7aa8bb2e18701c35571b186d4b26d65b0
madrenegade <madrenegade@gmx.de>
2011-08-06 22:24:01 +0200

    OPEN - task 6: Templating http://github.com/madrenegade/GitStatViewer/issues/issue/6

--- build.sbt
+++ build.sbt
@@ -9,7 +9,8 @@ resolvers += "JBoss Repos" at "http://repository.jboss.org/nexus/content/groups/
 libraryDependencies ++= Seq(
   "junit" % "junit" % "4.8.2" % "test->default",
   "jfree" % "jfreechart" % "1.0.13" % "compile->default",
-  "org.slf4j" % "slf4j-log4j12" % "1.6.1"
+  "org.slf4j" % "slf4j-log4j12" % "1.6.1",
+  "org.fusesource.scalate" % "scalate-core" % "1.5.0"% "compile->default"
 )
 
 scalacOptions in Compile ++= Seq(
--- src/main/scala/de/madsolutions/gitstatviewer/LogVisualizer.scala
+++ src/main/scala/de/madsolutions/gitstatviewer/LogVisualizer.scala
@@ -13,10 +13,14 @@ import scala.xml.EntityRef
 import scala.xml.dtd.DocType
 import scala.xml.dtd.SystemID
 
+import org.fusesource.scalate._
+
 class LogVisualizer(stats: Elem) {
   
   private val indexFile = "report.html"
   private val backHref = "../" + indexFile
+  
+  private val engine = new TemplateEngine
   
   protected val reporters: List[ReportGenerator] = List()
 
@@ -50,44 +54,13 @@ class LogVisualizer(stats: Elem) {
   private def reportUrl(name: String) = "./" + name + "/" + indexFile
 
   private def generatePage(title: String, body: Elem) = {
-    <html lang="en">
-      <head>
-        <title>{title}</title>
-        <style type="text/css">
-        	html, body, div, p {{
-        		margin: 0;
-        		padding: 0;
-        		border: 0;
-        	}}
+    val templateSource = TemplateSource.fromFile("template.mustache")
+    
+    val attributes = Map(
+        "title" -> title,
+        "body" -> body
+    )
 
-        	.main {{
-        		margin:0 auto; width:85%;
-        	}}
-
-        	.dp20, .dp25, .dp33, .dp50, .dp100 {{
-        		float:left; display: inline; *margin-left:-0.04em;
-        	}}
-
-        	.dp20{{width:20%;}}
-        	.dp25{{width:25%;}}
-        	.dp33{{width:33.33%;}}
-        	.dp50{{width:50%;}}
-        	.dp100{{width:100%;}}
-        	.clear{{ clear:both;}}
-        </style>
-      </head>
-      <body>
-        <div class="main">
-        	<div class="dp100"><h1>{title}</h1></div>
-        	<div class="dp100">{body}</div>
-        	<div class="dp100">
-        		<h3>
-        			<span style="font-style: italic">GitStatViewer</span>
-        			{EntityRef("#169")} 2011 Stefan Bradl
-        		</h3>
-        	</div>
-        </div>
-      </body>
-    </html>
+    XML.loadString(engine.layout(templateSource, attributes))
   }
 }
